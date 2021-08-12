75f4c0e28e001e8643c18822df127e206ea7e12c
Petar Shatanovski <p.shatanovski@gmail.com>
2021-08-12 15:03:10 +0200

    Add Stripe

---
+++ HiddenVilla_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51JNcMgGJ9UUVac2WQ6msph82u1yblSNDhirfQ9fF0zMZD87iwgBUf6iZGV6juccWa0RBUiSvm1HflfxqZnnjLAyK00BHNMTcCE');
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+};
